06c29501737e4efedd48454e8783c364e1d2dba2
Thiago Justo <tjusto2409@gmail.com>
2019-07-15 06:28:45 -0300

    Páginas internas do admin como post e usuario feito, alterado title e favicon

--- src/app/modules/module-admin/admin-routing.module.ts
+++ src/app/modules/module-admin/admin-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { UsuarioComponent } from './admin/usuario/usuario.component';
-import { PostsComponent } from '../module-post/posts/posts.component';
 import { AdminComponent } from './admin/admin.component';
 import { DefaultComponent } from './admin/default/default.component';
+import { PostsComponent } from './admin/posts/posts.component';
 
 const adminRoutes: Routes = [
   { path: 'admin', component: AdminComponent, children:[
     { path: '', component: DefaultComponent },
     { path: 'usuario', component: UsuarioComponent },
-    { path: 'post', component: PostsComponent },
+    { path: 'posts', component: PostsComponent },
   ]}
 ];
 
